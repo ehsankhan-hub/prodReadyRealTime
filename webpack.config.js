ec8e1c19481514c8c57c2271b4a572c98d8780ef
ehsan <ehsakhan@gmail.com>
2026-04-11 17:36:47 +0300

    stackblitz fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const path = require('path');
 module.exports = {
   resolve: {
     alias: {
-      '@int/geotoolkit': path.resolve(__dirname, '../../../bin/int-geotoolkit-5.0.58.tgz'),
-      '@int/geotoolkit3d': path.resolve(__dirname, '../../../bin/int-geotoolkit3d-5.0.58.tgz'),
-      '@int/impl': path.resolve(__dirname, '../../../bin/int-impl-5.0.58.tgz')
+      '@int/geotoolkit': path.resolve(__dirname, './libs/int-geotoolkit-5.0.58.tgz'),
+      '@int/geotoolkit3d': path.resolve(__dirname, './libs/int-geotoolkit3d-5.0.58.tgz'),
+      '@int/impl': path.resolve(__dirname, './libs/int-impl-5.0.58.tgz')
     }
   }
 };
